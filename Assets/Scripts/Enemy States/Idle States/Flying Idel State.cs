79fc490fe75cbf41e55488c9e2c7d200306a583d
ZachaTron007 <zacharyyates45@gmail.com>
2024-11-16 21:25:46 -0800

    Flying enemy idel roamimg state finished

--- Assets/Scripts/Enemy States/Idle States/Flying Idel State.cs
+++ Assets/Scripts/Enemy States/Idle States/Flying Idel State.cs
@@ -8,68 +8,59 @@ public class FlyingIdelState : BaseIdelState
     [SerializeField] private Sensors hitBoxScript;
     [SerializeField] private float idelSpeed;
     [SerializeField] private float waitTime = 2;
-    [SerializeField] private GameObject tempMarker;
     [SerializeField] private Vector2 dir;
+    private Vector2 boundsLowerLeft;
+    private Vector2 boundsUpperRight;
+
     private float timer;
-    bool arrived = false;
+    bool arrived = true;
+    private void Start() {
+        boundsLowerLeft = new Vector2(hitBoxScript.hitBox.bounds.min.x, hitBoxScript.hitBox.bounds.min.y);
+        boundsUpperRight = new Vector2(hitBoxScript.hitBox.bounds.max.x, hitBoxScript.hitBox.bounds.max.y);
+    }
     public override void Enter()
     {
         animator.Play(idelAniamtion.name);
-        PointPicker();
     }
 
     private Vector3 PointPicker() {
-        //Debug.DrawRay(unitVariables.transform.position, dir, Color.red, 1f);
         do {
-            Debug.Log(Physics2D.Raycast(unitVariables.transform.position, dir, HelperFunctions.PointToDistance(transform.position, point), HelperFunctions.layers["Level"]));
-            point = new Vector3(Random.Range(hitBoxScript.hitBox.bounds.min.x, hitBoxScript.hitBox.bounds.max.x), Random.Range(hitBoxScript.hitBox.bounds.min.x, hitBoxScript.hitBox.bounds.max.y), unitVariables.transform.position.z);
-            Debug.DrawRay(unitVariables.transform.position, dir, Color.red, .3f);
+            point = new Vector3(Random.Range(boundsLowerLeft.x, boundsUpperRight.x), Random.Range(boundsLowerLeft.y, boundsUpperRight.y), unitVariables.transform.position.z);
             dir = (point - unitVariables.transform.position).normalized;
+
         }
-        while (!ShootCheckRay());
+        while (ShootCheckRay());
+        
         arrived = false;
-        Instantiate(tempMarker, point, Quaternion.identity);
         return point;
     }
 
     public override void UpdateState() {
-        
-        if (!arrived) {/*
-            Debug.Log("point: " + point);
-            Debug.Log("dir: " + dir);
-            Debug.Log("Flip?: " + (point.x < unitVariables.transform.position.x));*/
-            unitVariables.sr.flipX = point.x < unitVariables.transform.position.x;
-            rb.linearVelocity = dir * idelSpeed * Time.deltaTime;
-            if (Vector2.Distance(unitVariables.transform.position, point) < 0.1f) {
+        if (!arrived) {
+            unitVariables.SetDirection((dir.x > 0) ? 1 : -1);
+            rb.linearVelocity = idelSpeed * Time.deltaTime * dir;
+            if (Vector2.Distance(unitVariables.transform.position, point) < 0.2f) {
                 arrived = true;
             }
         } else {
-            arrived = true;
-            unitVariables.transform.position = point;
             rb.linearVelocity = Vector2.zero;
             timer +=Time.deltaTime;
             if (timer > waitTime) {
                 PointPicker();
                 timer = 0;
             }
-            //Invoke(nameof(PointPicker), waitTime);
             
         }
     }
     private bool ShootCheckRay() {
-        RaycastHit2D hit = Physics2D.BoxCast(unitVariables.transform.position, unitVariables.mainCollider.hitBox.size,0,dir, HelperFunctions.PointToDistance(transform.position, point), HelperFunctions.layers["Level"]);
         Vector2 offset = unitVariables.mainCollider.hitBox.offset;
-        Vector2 size = unitVariables.mainCollider.hitBox.size;
+        Vector2 size = unitVariables.mainCollider.hitBox.size/2;
+        //Debug.Log("other layermask: " + layerMask);
         Vector3 startPos = new Vector3(unitVariables.transform.position.x + offset.x, unitVariables.transform.position.y + offset.y, unitVariables.transform.position.z);
-        RaycastHit2D topRight = Physics2D.Raycast(new Vector3(startPos.x + size.x, startPos.y + size.y, startPos.z), dir, HelperFunctions.PointToDistance(transform.position, point), HelperFunctions.layers["Level"]);
-        Debug.DrawLine(new Vector3(startPos.x + size.x, startPos.y + size.y, startPos.z),point,Color.blue,1f);
-        RaycastHit2D topLeft = Physics2D.Raycast(new Vector3(startPos.x - size.x, startPos.y + size.y, startPos.z), dir, HelperFunctions.PointToDistance(transform.position, point), HelperFunctions.layers["Level"]);
-        Debug.DrawLine(new Vector3(startPos.x - size.x, startPos.y + size.y, startPos.z), point, Color.blue, 1f);
-        RaycastHit2D bottomLeft = Physics2D.Raycast(new Vector3(startPos.x - size.x, startPos.y - size.y, startPos.z), dir, HelperFunctions.PointToDistance(transform.position, point), HelperFunctions.layers["Level"]);
-        Debug.DrawLine(new Vector3(startPos.x - size.x, startPos.y - size.y, startPos.z), point, Color.blue, 1f);
-        RaycastHit2D bottomRight = Physics2D.Raycast(new Vector3(startPos.x+size.x,startPos.y-size.y,startPos.z), dir, HelperFunctions.PointToDistance(transform.position, point), HelperFunctions.layers["Level"]);
-        Debug.DrawLine(new Vector3(startPos.x + size.x, startPos.y - size.y, startPos.z), point, Color.blue, 1f);
-        
+        RaycastHit2D topRight = Physics2D.Raycast(new Vector3(startPos.x * size.x, startPos.y * size.y, startPos.z), dir, HelperFunctions.PointToDistance(transform.position, point), HelperFunctions.LayerMaskCreator(new int[] { HelperFunctions.layers["Level"] }));
+        RaycastHit2D topLeft = Physics2D.Raycast(new Vector3(startPos.x * -size.x, startPos.y * size.y, startPos.z), dir, HelperFunctions.PointToDistance(transform.position, point), HelperFunctions.LayerMaskCreator(new int[] { HelperFunctions.layers["Level"] }));
+        RaycastHit2D bottomLeft = Physics2D.Raycast(new Vector3(startPos.x * -size.x, startPos.y * -size.y, startPos.z), dir, HelperFunctions.PointToDistance(transform.position, point), HelperFunctions.LayerMaskCreator(new int[] { HelperFunctions.layers["Level"] }));
+        RaycastHit2D bottomRight = Physics2D.Raycast(new Vector3(startPos.x * size.x,startPos.y * -size.y,startPos.z), dir, HelperFunctions.PointToDistance(transform.position, point), HelperFunctions.LayerMaskCreator(new int[] { HelperFunctions.layers["Level"] }));
         return topRight||topLeft||bottomRight||bottomLeft;
     }
 }
--- Assets/Scripts/Enemy States/Idle States/Ledge Turn Idel State.cs
+++ Assets/Scripts/Enemy States/Idle States/Ledge Turn Idel State.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class LedgeTurnIdelState : IdelState
 {
     public override void UpdateState() {
+        //Debug.Log("Is ground in front: " + unitVariables.IsGroundInFront());
         if(!unitVariables.IsGroundInFront()) {
             unitVariables.SetDirection(-unitVariables.GetDirection());
         }
